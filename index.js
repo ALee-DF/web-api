a6284bff28e74ddf956c8894858337431933535e
Alexander Lee <alex8usa8@gmail.com>
2017-10-07 21:50:32 -0700

    Registered a route on the Express app for GET requests to /notepad. Connected to the "library" database of MongoDB server. #1

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const express = require('express')
+const { MongoClient } = require('mongodb')
+const bodyParser = require('body-parser')
+const url = 'mongodb://localhost/library'
 const app = express()
+app.use(bodyParser.json())
+
+app.get('/notepad', (req, res) => {
+  MongoClient.connect(url, (err, db) => {
+    if (err) {
+      console.error(err)
+      res.sendStatus(500)
+      process.exit(1)
+    }
+  })
+})
+
 app.listen(3000, () => {
   console.log('Web Api Listening on Port 3000!')
 })
